6ba2197d361dc54b2b8cfd5ce7e6710a0d03e3a8
Sprixitite <91488389+Sprixitite@users.noreply.github.com>
2023-04-08 21:21:01 +0100

    Alter UIExtensions to allow for non-panel bases

--- UIExtension.cs
+++ UIExtension.cs
@@ -7,20 +7,23 @@ namespace WinForMono {
     public class UIExtension : UIElement {
 
         public UIExtension() : base(false) {
-            panel = new UIPanel();
+            extension_base = new UIPanel();
         }
 
-        public override UIPosition position { get => panel.position; set => panel.position = value; }
-        public override UIPosition size { get => panel.size; set => panel.size = value; }
-        public override UIAnchor anchor { get => panel.anchor; set => panel.anchor = value; }
-        public override bool copy_axis { get => panel.copy_axis; set => panel.copy_axis = value; }
-        public override Control underlying { get => panel.underlying; set => panel.underlying = value; }
-        public override DrawStyle draw_style { get => panel.draw_style; set => panel.draw_style = value; }
-        public override int corner_radius { get => panel.corner_radius; set => panel.corner_radius = value; }
-        public Color background_colour { get => panel.colour; set => panel.colour = value; }
-        public override WinformWrapper parent { get => panel.parent; set => panel.parent = value; }
+        public override UIPosition position { get => extension_base.position; set => extension_base.position = value; }
+        public override UIPosition size { get => extension_base.size; set => extension_base.size = value; }
+        public override UIAnchor anchor { get => extension_base.anchor; set => extension_base.anchor = value; }
+        public override bool copy_axis { get => extension_base.copy_axis; set => extension_base.copy_axis = value; }
+        public override Control underlying { get => extension_base.underlying; set => extension_base.underlying = value; }
+        public override DrawStyle draw_style { get => extension_base.draw_style; set => extension_base.draw_style = value; }
+        public override int corner_radius { get => extension_base.corner_radius; set => extension_base.corner_radius = value; }
+        // public Color background_colour { get => extension_base.colour; set => extension_base.colour = value; }
+        public override WinformWrapper parent { get => extension_base.parent; set => extension_base.parent = value; }
 
-        protected UIPanel panel;
+        public UIElement extension_base {
+            get;
+            protected set;
+        }
 
     }
 
